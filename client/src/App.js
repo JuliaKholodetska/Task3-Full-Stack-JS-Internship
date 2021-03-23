d65ae74ef0d48cd13391c86d1949082b214e7769
Julia Kholodetska <j.kholodetska@gmail.com>
2021-03-23 19:47:13 +0200

    Improve design.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="index.html">Beauty Beach</a>
+            <a className="brand" href="/">
+              Beauty Beach
+            </a>
           </div>
-          <div>
-            <a href="/cart">Cart</a>
+          <div id="navbar-button">
+            <a className="fas fa-shopping-cart" href="/cart"></a>
             <a href="/signin">Sign In</a>
           </div>
         </header>
