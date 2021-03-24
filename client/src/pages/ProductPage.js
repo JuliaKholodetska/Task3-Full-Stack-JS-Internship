3b9cecb4321ef35d0479910f2adb3d35473eabc9
Julia Kholodetska <j.kholodetska@gmail.com>
2021-03-24 15:21:17 +0200

    code style

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -19,6 +19,7 @@ export default function ProductPage(props) {
     const addToCartHandler = () => {
         props.history.push(`/cart/${productId}?qty=${qty}`);
     };
+
     return (
         <div>
             { loading ? (
