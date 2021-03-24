e3d75f74fd8225892ecb1c695b4d738ca422641f
Julia Kholodetska <j.kholodetska@gmail.com>
2021-03-24 23:54:10 +0200

    Edit style code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import ProductPage from './pages/ProductPage';
-import HomePage from './pages/HomePage';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              Beauty Beach
-            </a>
-          </div>
-          <div id="navbar-button">
-            <a className="fas fa-shopping-cart" href="/cart"></a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <Route path="/product/:id" component={ProductPage}></Route>
-          <Route path="/" component={HomePage} exact></Route>
-        </main>
-        <footer className="row center">All rights reserved</footer>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="row">
+					<div>
+						<a className="brand" href="/">
+							Beauty Beach
+						</a>
+					</div>
+					<div id="navbar-button">
+						<a className="fas fa-shopping-cart" href="/cart"></a>
+						<a href="/signin">Sign In</a>
+					</div>
+				</header>
+				<main>
+					<Route path="/product/:id" component={ProductPage}></Route>
+					<Route path="/" component={HomePage} exact></Route>
+				</main>
+				<footer className="row center">All rights reserved</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,20 @@
-import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
 import {
-  productDetailsReducer,
-  productListReducer,
-} from './reducers/productReducers';
+	productDetailsReducer,
+	productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 const reducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+	reducer,
+	initialState,
+	composeEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
