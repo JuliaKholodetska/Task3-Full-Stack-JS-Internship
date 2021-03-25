ecbf26c04647518e4a6172054baa21a8c3294f53
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-03-25 14:41:26 +0200

    install mongoose

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import ProductPage from "./pages/ProductPage";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+
 	return (
 		<BrowserRouter>
 			<div className="grid-container">
 				<header className="row">
 					<div>
-						<a className="brand" href="/">
+						<Link className="brand" to="/">
 							Beauty Beach
-						</a>
+						</Link>
 					</div>
 					<div>
-						<a href="/cart">Cart</a>
-						<a href="/signin">Sign In</a>
+						<Link className="fas fa-shopping-cart" to="/cart">
+							{cartItems.length > 0 && (
+								<span className="badge">{cartItems.length}</span>
+							)}
+						</Link>
+						<Link to="/signin">Sign In</Link>
 					</div>
 				</header>
 				<main>
+					<Route path="/cart/:id?" component={CartPage}></Route>
 					<Route path="/product/:id" component={ProductPage}></Route>
 					<Route path="/" component={HomePage} exact></Route>
 				</main>
